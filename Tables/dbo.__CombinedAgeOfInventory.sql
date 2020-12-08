ad41c96867a74891a1aeecdace4956cd76fe1f8b
jessica-ramos <jessica.ramos@red-gate.com>
2020-12-07 18:48:23 -0800

    Update dbo.__CombinedAgeOfInventory.sql

--- Tables/dbo.__CombinedAgeOfInventory.sql
+++ Tables/dbo.__CombinedAgeOfInventory.sql
@@ -3,3 +3,10 @@ CREATE TABLE [dbo].[__CombinedAgeOfInventory]
 [testcol2] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
+
+  
+CREATE TABLE [dbo].[__CombinedAgeOfInventory]
+(
+[testcol2] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
